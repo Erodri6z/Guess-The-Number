b5bbae8cf7f76e636e38798597cadc39f00d2214
Erodri6z <erikrodriguez2417@gmail.com>
2022-05-29 17:53:31 -0500

    complete task 1 and 2

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    secretNum: null,
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuess: [],
+  title: 'Guess the Number!',
+  getGuess: window.prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`),
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     }
-  }
 
-  test test test 
+  }
